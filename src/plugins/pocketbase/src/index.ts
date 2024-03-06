dfd4236679d9fce516ed0becaf7715f386908e17
Ben Allfree <ben@benallfree.com>
2024-03-05 21:50:35 -0800

    chore: remove unused pocketbase plugin

--- src/plugins/pocketbase/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { GobotBase, GobotOptions } from '../../../GobotBase'
-
-export class PocketBase extends GobotBase {
-  constructor(options: Partial<GobotOptions> = {}) {
-    super(`pocketbase/pocketbase`, options)
-  }
-}
