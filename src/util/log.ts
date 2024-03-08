a2492c3b11ecc69fa711187c95dfce1df85a3f50
Ben Allfree <ben@benallfree.com>
2024-03-08 13:27:17 -0800

    feat: enhanced logging methods

--- src/util/log.ts
+++ src/util/log.ts
@@ -1,4 +1,12 @@
 import { verbosity } from '../settings'
 
 export const dbg = (...args: any[]) => verbosity() > 1 && console.log(...args)
+export const dbge = (...args: any[]) =>
+  verbosity() > 1 && console.error(...args)
 export const info = (...args: any[]) => verbosity() > 0 && console.log(...args)
+export const infoe = (...args: any[]) =>
+  verbosity() > 0 && console.error(...args)
+export const rdbg = (s: string) => verbosity() > 1 && process.stdout.write(s)
+export const rdbge = (s: string) => verbosity() > 1 && process.stderr.write(s)
+export const rinfo = (s: string) => verbosity() > 0 && process.stdout.write(s)
+export const rinfoe = (s: string) => verbosity() > 0 && process.stderr.write(s)
