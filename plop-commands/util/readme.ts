ab57f7c1bea4442134ae245fdaa068abba80972f
Ben Allfree <ben@benallfree.com>
2024-03-15 19:41:36 -0700

    enh: update to point to npm as primary lander

--- plop-commands/util/readme.ts
+++ plop-commands/util/readme.ts
@@ -16,7 +16,7 @@ export const availableAppsMd = markdownTable([
     const branch = getCurrentGitBranch()
     console.log({ branch })
     const { name, description, homepage, slug } = app
-    const readmeUrl = `https://github.com/benallfree/gobot/tree/${branch}/src/apps/${name}/helper/readme.md`
+    const readmeUrl = `https://www.npmjs.com/package/gobot-{{name}}`
     const logoUrl = `https://raw.githubusercontent.com/benallfree/gobot/${branch}/src/apps/${name}/logo-50x.png`
     return [
       mkLink(`<img src="${logoUrl}">`, homepage),
--- plop-templates/app/sample-project/readme.md
+++ plop-templates/app/sample-project/readme.md
@@ -4,4 +4,4 @@ Features:
 
 - Demo `"bin"` script
 - Shows how to run `{{name}}` programmatically using the [Gobot API](https://github.com/benallfree/gobot/tree/{{branch}}/docs/readme.md).
-- Shows how to add `gobot-{{name}}` as a version-locked dependency.
+- Shows how to add [gobot-{{name}}](https://www.npmjs.com/package/gobot-{{name}}) as a version-locked dependency.
