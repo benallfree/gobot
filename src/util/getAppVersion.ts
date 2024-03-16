f987e580b79e08d8700e81a7049d1d8149ae3d22
Ben Allfree <ben@benallfree.com>
2024-03-15 20:45:35 -0700

    fix: module search casing

--- src/util/getAppVersion.ts
+++ src/util/getAppVersion.ts
@@ -1,6 +1,6 @@
 export async function getAppVersion(name: string): Promise<string> {
   try {
-    const moduleName = `gobot-${name}`
+    const moduleName = `gobot-${name.toLocaleLowerCase()}`
     const module = await import(moduleName)
     return module.meta.version || '*'
   } catch (error) {
