afd2c4c7513340df698524d26c483c8f35d9966a
Ben Allfree <ben@benallfree.com>
2024-03-19 09:38:25 -0700

    enh: improve url extraction

--- src/util/extractUserAndRepo.ts
+++ src/util/extractUserAndRepo.ts
@@ -1,12 +1,13 @@
 export function extractUserAndRepo(url: string) {
   const regex = /^(?:https?:\/\/)?(?:github\.com\/|github:)?([^/]+)\/([^/]+).*$/
   const match = url.match(regex)
-  if (match) {
-    return {
-      user: match[1],
-      repo: match[2],
-    }
+  if (!match) throw new Error(`Could not extract <user>/<repo> from ${url}`)
+  const user = match[1]
+  const repo = match[2]
+  if (!user) throw new Error(`Could not extract <user> from ${url}`)
+  if (!repo) throw new Error(`Could not extract <repo> from ${url}`)
+  return {
+    user,
+    repo,
   }
-
-  throw new Error(`No match found for ${url}`)
 }
