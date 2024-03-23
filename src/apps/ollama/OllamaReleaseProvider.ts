068c20301a2751c646f69d4f3007782d95f70806
Ben Allfree <ben@benallfree.com>
2024-03-23 04:00:18 -0700

    chore: remove unused ollama overrides

--- src/apps/ollama/OllamaReleaseProvider.ts
+++ src/apps/ollama/OllamaReleaseProvider.ts
@@ -1,6 +1,5 @@
 import { basename } from 'path'
 import { GithubReleaseProvider } from '../../GithubReleaseProvider'
-import { ArchKey, PlatformKey } from '../../Gobot'
 
 export class OllamaReleaseProvider extends GithubReleaseProvider {
   get slug() {
@@ -9,14 +8,5 @@ export class OllamaReleaseProvider extends GithubReleaseProvider {
 
   isArchiveUrlAllowed(url: string) {
     return !basename(url).includes(`.`)
-  }
-
-  platformRegex(arch: ArchKey, aliases: string[]) {
-    return new RegExp(`[_-](?:${aliases.join(`|`)})(?:[_\\-.]|$)`, 'i')
-  }
-
-  archRegex(os: PlatformKey, aliases: string[]) {
-    if (os === 'darwin') return new RegExp(``)
-    return new RegExp(`[_-](?:${aliases.join(`|`)})(?:[_\\-.]|$)`, 'i')
   }
 }
