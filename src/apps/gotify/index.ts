538e996e8aec8fe13e0bd3e66f0a0ec707f59975
Ben Allfree <ben@benallfree.com>
2024-04-22 17:17:07 -0700

    fix: gotify provider

--- src/apps/gotify/index.ts
+++ src/apps/gotify/index.ts
@@ -1,10 +1,9 @@
 import type { AppInfo } from '..'
-import { mkGotify } from './overrides'
 
 export const gotify: AppInfo = {
   name: 'Gotify',
   description: `A simple server for sending and receiving messages in real-time per WebSocket. (Includes a sleek web-ui)`,
   homepage: `https://gotify.net`,
   isAlpha: true,
-  factory: mkGotify,
+  factory: `gotify/server`,
 }
--- src/apps/gotify/overrides.ts
+++ src/apps/gotify/overrides.ts
@@ -1,32 +1,13 @@
-import { chmodSync } from 'fs'
 import type { AppFactory } from '..'
 import { GithubReleaseProvider } from '../../GithubReleaseProvider'
 import { Gobot } from '../../Gobot'
 
 export const GOTIFY_REPO = 'gotify/server'
 
-class gotify_GithubReleaseProvider extends GithubReleaseProvider {
-  get className() {
-    return `gotify_GithubReleaseProvider`
-  }
-}
-
-class gotify_Gobot extends Gobot {
-  get className(): string {
-    return `gotify_Gobot`
-  }
-
-  async unpack(downloadPath: string, version: string) {
-    if (this.os !== 'win32') {
-      chmodSync(downloadPath, '755')
-    }
-  }
-}
-
 export const mkGotify: AppFactory = (optionsIn) => {
-  return new gotify_Gobot(
+  return new Gobot(
     GOTIFY_REPO,
-    (root, cacheRoot) => new gotify_GithubReleaseProvider(root, cacheRoot),
+    (root, cacheRoot) => new GithubReleaseProvider(root, cacheRoot),
     optionsIn,
   )
 }
