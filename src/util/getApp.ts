68594287a612a93cc916b0078437db31c4a37d96
Ben Allfree <ben@benallfree.com>
2024-04-22 18:15:34 -0700

    wip

--- src/util/getApp.ts
+++ src/util/getApp.ts
@@ -12,7 +12,7 @@ export const appsRoot = mkSetting(join(__dirname, `..`, `apps`))
 export const getApp = async (slug: string) => {
   try {
     console.log({ slug, appsRoot: appsRoot() })
-    console.log(globSync(join(appsRoot())))
+    console.log(globSync(join(appsRoot(), '**')))
     const path = join(appsRoot(), slug, 'index.js')
     const module = await import(path)
     return module[slug] as AppInfo
