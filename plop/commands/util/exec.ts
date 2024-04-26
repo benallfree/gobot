8dcd32e7a158f3bfd16124353c1f697abda028fb
Ben Allfree <ben@benallfree.com>
2024-04-26 04:38:31 -0700

    wip

--- plop/commands/util/exec.ts
+++ plop/commands/util/exec.ts
@@ -1,7 +1,11 @@
 import { delimiter, join } from 'path'
+import { platform } from 'process'
 import { Flags } from '../../../src/util/flags'
 import { spawn } from '../../../src/util/spawn'
 import { GOBOT_TEST_CACHE_ROOT_NPM } from '../../run'
+
+// https://github.com/nodejs/node/issues/20605#issuecomment-387489708
+const path = () => (platform === 'win32' ? `Path` : `PATH`)
 
 export const exec: typeof spawn = async (cmd, _options, onProc) => {
   const env = { ...process.env }
@@ -11,17 +15,17 @@ export const exec: typeof spawn = async (cmd, _options, onProc) => {
     ..._options,
     env: {
       ...env,
-      PATH: !env[Flags.UseNpm]
+      [path()]: !env[Flags.UseNpm]
         ? [
             GOBOT_TEST_CACHE_ROOT_NPM,
             join(GOBOT_TEST_CACHE_ROOT_NPM, `bin`),
-            env.PATH,
+            env[path()],
           ].join(delimiter)
-        : env.PATH,
+        : env[path()],
       ..._options?.env,
     },
   }
-  console.log({ PATH: options.env.PATH })
+  console.log({ env })
   const code = await spawn(cmd, options, onProc)
   return code
 }
